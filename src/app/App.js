f51624f50748b1d8e3e30ec4d2ed0144cd8277e6
ashujainvi <ajainvi@buffalo.edu>
2020-04-16 18:48:40 -0400

    adding sass, changing project structure

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Clock from './components/clock/clock.js';
+import logo from '../logo.svg';
+import './App.scss';
+import Clock from '../components/clock/clock.js';
 
 class App extends React.Component {
   render() {
