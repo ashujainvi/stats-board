66f35051dc75b0bfa3409df69b226e7f24e7a136
ashujainvi <ajainvi@buffalo.edu>
2020-04-16 18:19:16 -0400

    adding tick to clock state.

--- src/components/clock/clock.js
+++ src/components/clock/clock.js
@@ -6,6 +6,20 @@ class Clock extends React.Component {
         this.state = {date: new Date()};
     }
 
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+          );
+    }
+  
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({ date: new Date()});
+    }
     render() {
         return (
             <p>It is {this.state.date.toLocaleTimeString()}.</p>
