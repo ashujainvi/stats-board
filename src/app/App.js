ede93fc059ce99d9dd16b3efb95a3b0a95e6164a
ashujainvi <ajainvi@buffalo.edu>
2020-04-19 16:21:08 -0400

    added color variables and font

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import Header from '../components/header/header.js';
+import Header from 'components/header/header.js';
 
 class App extends React.Component {
   render() {
