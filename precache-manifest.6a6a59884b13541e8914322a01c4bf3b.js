06775d1fb741027a8699f334900196b07aeebd68
ashujainvi <ajainvi@buffalo.edu>
2020-04-19 19:48:20 -0400

    Updates

---
+++ precache-manifest.6a6a59884b13541e8914322a01c4bf3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdfb83807e79786af3aa930bff5b2bc7",
+    "url": "/stats-board/index.html"
+  },
+  {
+    "revision": "3e63c37975a802dd0bb6",
+    "url": "/stats-board/static/css/main.762a4f6a.chunk.css"
+  },
+  {
+    "revision": "f420b996ff2b6eac1b3f",
+    "url": "/stats-board/static/js/2.9a590e8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stats-board/static/js/2.9a590e8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e63c37975a802dd0bb6",
+    "url": "/stats-board/static/js/main.0722423f.chunk.js"
+  },
+  {
+    "revision": "1376ed48b12ec0864e30",
+    "url": "/stats-board/static/js/runtime-main.fd80ab5b.js"
+  },
+  {
+    "revision": "b250a40f6070eaf35b5218ec03247e91",
+    "url": "/stats-board/static/media/stats-board-logo.b250a40f.svg"
+  }
+]);
