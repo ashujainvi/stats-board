50b20e8cef6533932a334bb149edfb71efb6f419
ashujainvi <ajainvi@buffalo.edu>
2020-04-20 00:53:13 -0400

    adding circles canvas

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './Hero.scss';
+import Canvas from 'components/Canvas/Canvas';
 
 class Hero extends React.Component {
-    constructor(props) {
-        super(props);
-        // this.state = {date: new Date()};
-    }
-
     render() {
         return (
             <div className="hero-container">
+                <Canvas/>
                 <h1>All your data available in one central hub</h1>
                 <p>Our tool allows you to put all your data at your fingertips within a single central hub.   
-                     <span className="color-blue">Stats Board</span> improves the quality of your data by harmonizing all datasets into a consistent format.</p>
-                     <button className="button button-primary">Get Started</button>
+                     <span className="color-blue">Stats Board</span> improves the quality of your data by harmonizing all datasets into a consistent format.
+                </p>
+                <button className="button button-primary">Get Started</button>
             </div>
         )
     }
