a5297b492b065537616068a1a81d4f3e96d1770c
ashujainvi <ajainvi@buffalo.edu>
2020-04-22 21:02:51 -0400

    added blob to canvas

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -17,6 +17,8 @@ class Canvas extends React.Component {
             canvas.height = windowHeight;
             initCircles();
         });
+
+        // circles for canvas
         let circlesArray = [];
         function initCircles() {
             circlesArray = [];
@@ -44,18 +46,31 @@ class Canvas extends React.Component {
                 return Math.random() * (windowHeight - radius * 2) + radius;
             }
         }
+
+        // blob for canvas
+        const blobConfig = {
+            backgroundColor: '#06d294',
+            ctx
+        }
+        const blob = new Blob(blobConfig)
         
         function animateCircles() {
-            requestAnimationFrame(animateCircles);
             ctx.clearRect(0, 0, windowWidth, windowHeight);
+            blob.update();
+            ctx.clip();
 
             circlesArray.forEach( circle => {
                 circle.update();
-            })
+            });
+            ctx.restore();
+
+            requestAnimationFrame(animateCircles);
         }
+
 
         initCircles();
         animateCircles();
+        // animateBlob();
     }
     
     render() {
@@ -75,7 +90,7 @@ class Circle {
         this.radius = data.radius;
         this.maxRadius = data.maxRadius || 30;
         this.minRadius = data.minRadius || 2;
-        this.colorRange = data.colorRange || [ '#fd413c', '#06d294', '#333333'];
+        this.colorRange = data.colorRange || [ '#fd413c', '#333333'];
         this.color = this.colorRange[Math.floor(Math.random() * this.colorRange.length)];
         this.mouse = {
             x: null,
@@ -128,6 +143,103 @@ class Circle {
             this.mouse.y = event.y;
         })
     }
+}
+
+class Blob {
+    constructor(config) {
+        this.ctx = config.ctx;
+        this.backgroundColor = config.backgroundColor;
+        this.TWO_PI = Math.PI * 2;
+        this.HALF_PI = Math.PI / 2;
+        this.wobbleIncrement = 0;
+        // use this to change the size of the blob
+        this.radius = config.size || 700;
+        // think of this as detail level
+        // number of conections in the `bezierSkin`
+        this.segments = 12;
+        this.step = this.HALF_PI / this.segments;
+        this.anchors = [];
+        this.radii = [];
+        this.thetaOff = [];
+    
+        const bumpRadius = 80;
+        const halfBumpRadius = bumpRadius / 2;
+    
+        for (let i = 0; i < this.segments + 2; i++) {
+          this.anchors.push(0, 0);
+          this.radii.push(Math.random() * bumpRadius - halfBumpRadius);
+          this.thetaOff.push(Math.random() * 2 * Math.PI);
+        }
+    
+        this.theta = 0;
+        this.thetaRamp = 0;
+        this.thetaRampDest = 12;
+        this.rampDamp = 25;
+    }
+
+    update() {
+        this.thetaRamp += (this.thetaRampDest - this.thetaRamp) / this.rampDamp;
+        this.theta += 0.03;
+    
+        this.anchors = [0, this.radius];
+        for (let i = 0; i <= this.segments + 2; i++) {
+          const sine = Math.sin(this.thetaOff[i] + this.theta + this.thetaRamp);
+          const rad = this.radius + this.radii[i] * sine;
+          const x = rad * Math.sin(this.step * i);
+          const y = rad * Math.cos(this.step * i);
+          this.anchors.push(x, y);
+        }
+    
+        this.ctx.save();
+        this.ctx.translate(-5, -5);
+        this.ctx.scale(1, 1);
+        this.ctx.fillStyle = this.backgroundColor;
+        this.ctx.beginPath();
+        this.ctx.moveTo(0, 0);
+        this.bezierSkin(this.anchors, false);
+        
+        this.ctx.lineTo(0, 0);
+        this.ctx.fill();
+    }
+
+
+    // array of xy coords, closed boolean
+    bezierSkin(bez, closed = true) {
+        const avg = this.calcAvgs(bez);
+        const leng = bez.length;
+    
+        if (closed) {
+            this.ctx.moveTo(avg[0], avg[1]);
+            for (let i = 2; i < leng; i += 2) {
+                let n = i + 1;
+                this.ctx.quadraticCurveTo(bez[i], bez[n], avg[i], avg[n]);
+            }
+        this.ctx.quadraticCurveTo(bez[0], bez[1], avg[0], avg[1]);
+        } else {
+            this.ctx.moveTo(bez[0], bez[1]);
+            this.ctx.lineTo(avg[0], avg[1]);
+            for (let i = 2; i < leng - 2; i += 2) {
+                let n = i + 1;
+                this.ctx.quadraticCurveTo(bez[i], bez[n], avg[i], avg[n]);
+            }
+            this.ctx.lineTo(bez[leng - 2], bez[leng - 1]);
+        }
+    }
+
+    // create anchor points by averaging the control points
+    calcAvgs(p) {
+        const avg = [];
+        const leng = p.length;
+        let prev;
+    
+        for (let i = 2; i < leng; i++) {
+        prev = i - 2;
+        avg.push((p[prev] + p[i]) / 2);
+        }
+        // close
+        avg.push((p[0] + p[leng - 2]) / 2, (p[1] + p[leng - 1]) / 2);
+        return avg;
+  }
 }
 
 export default Canvas;
